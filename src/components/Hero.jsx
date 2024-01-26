8d5d89feeced41431c92d6a41af6fad60a3c419a
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-01-26 16:58:45 +0530

    Added custom cursors and framer motion to links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from '../style'
-import { title } from '../assets'
+
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}  mt-12`}>
