3d55e213560109455c1aac844c551defebdbc1c9
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-01-26 17:36:43 +0530

    added motion

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -8,8 +8,8 @@ const Socials = () => {
     <div className='flex justify-between w-screen items-center px-12 py-12 mt-40'>
       <div className={`flex gap-28`}>
         <a href="https://github.com/SidhantCodes" target="_blank"><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={github} alt="github" className={`w-[45px] h-[45px]`} /></a>
-        <a href="https://www.linkedin.com/in/smishra0110/" target="_blank"_blank><img src={linkedin} alt="linkedin" className={`w-[45px] h-[45px]`} /></a>
-        <a href="https://www.instagram.com/_sidhant22/" target="_blank"><img  src={instagram} alt="instagram" className={`w-[45px] h-[45px]`} /></a>
+        <a href="https://www.linkedin.com/in/smishra0110/" target="_blank"_blank><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={linkedin} alt="linkedin" className={`w-[45px] h-[45px]`} /></a>
+        <a href="https://www.instagram.com/_sidhant22/" target="_blank"><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={instagram} alt="instagram" className={`w-[45px] h-[45px]`} /></a>
       </div>
 
     
