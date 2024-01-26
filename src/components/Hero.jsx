11a5facb42f8908fc10719d543dcddbf8f8d9c1e
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-01-26 22:49:05 +0530

    Hero sec responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,19 +4,19 @@ import styles from '../style'
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}  mt-12`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 `}>
             
         <div className="flex flex-row justify-between items-center w-full px-12">
 
-          <h1 className="title flex-1 ss:text-[102px] text-[82px] text-black ss:leading-[110.8px] leading-[95px]">
+          <h1 className="title flex-1 text-[45px] leading-[40px] sm:text-[92px] sm:leading-[90.5px]">
             Hello 👋, I’m {" "}
             <br className="sm:block hidden"/>Sidhant Mishra 👾
           </h1>
         </div>  
-        <p className={`sub  max-w-[800px] mt-6 px-[51.3px]`}> Python Developer | Front-end Developer | Graphic Designer </p>
+        <p className={`sub max-w-[500px] text-[13.3px] sm:max-w-[800px] mt-6 px-12 sm:text-[25px]`}> Python Developer | Front-end Developer | Graphic Designer </p>
       </div>
-      <div className='px-12 flex items-center'>
-      Enthusiastic Python developer, seamlessly blending creativity.<br/> Exploring the nuances of frontend web development. I'm eager <br/> to turn innovative ideas into exceptional digital experiences.
+      <div className='px-[3rem] pt-20 text-[13px] justify-center sm:flex items-center sm:text-[15px]'>
+      Enthusiastic Python developer, seamlessly blending creativity.<br/> Exploring the nuances of development. I'm eager to turn <br />innovative ideas into exceptional digital experiences.
       </div>      
     </section>
   )
