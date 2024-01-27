dc075dfc9a7e5988d9d1dbca70a434f545408ef1
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-01-27 20:17:05 +0530

    Fixed icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,9 +9,9 @@ const Contact = () => {
         <p className='para2 px-12 flex-wrap pt-6 text-[15px] sm:text-[28px]'>Let's connect and kickstart the journey of transforming your ideas into tangible, <br /> innovative solutions! Reach out to get started.</p>
         <h1 className='bigtitle text-[50px] sm:text-[92px] mt-5'>sidhantmishra</h1>
         <div className={`flex gap-5 mt-1`}>
-          <a href="https://github.com/SidhantCodes" target="_blank"><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={githubO} alt="" className={`h-[35px] w-[35px]`} /></a>
-          <a href="https://www.linkedin.com/in/smishra0110/" target="_blank"><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={linkedinO} alt="" className={`h-[35px] w-[35px]`} /></a>
-          <a href="mailto:mishrasidhant01@gmail.com"><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={mail} alt="" className={`h-[35px] w-[35px]`} /></a>
+          <a href="https://github.com/SidhantCodes" target="_blank"><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={githubO} alt="" className={`h-[35px] w-[35px] sm:w-[55px] sm:h-[55px]`} /></a>
+          <a href="https://www.linkedin.com/in/smishra0110/" target="_blank"><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={linkedinO} alt="" className={`h-[35px] w-[35px] sm:w-[55px] sm:h-[55px]`} /></a>
+          <a href="mailto:mishrasidhant01@gmail.com"><motion.img whileHover={{ scale: [null, 1.1, 1.1] }} src={mail} alt="" className={`h-[35px] w-[35px] sm:w-[55px] sm:h-[55px]`} /></a>
         </div>
       </div>
 
