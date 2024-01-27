b42d5fa8e4abe06b5ffd3baca4af979ec97a6896
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-01-27 11:13:03 +0530

    Working on projects component and hiding scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Hero, Navbar, Socials } from './components'
+import { Hero, Navbar, Socials, Projects } from './components'
+
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero/>
       <Socials/>
+      <Projects/>
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero"
 import Socials from "./Socials"
+import Projects from "./Projects";
 export {
   Navbar,
   Hero,
-  Socials
+  Socials,
+  Projects,
 };
